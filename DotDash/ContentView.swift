b35156d690d145d3dee9c73213c942c229677e61
Tiago Oliveira <tsabian@hotmail.com>
2025-09-22 21:23:22 -0300

    Initial Commit

---
+++ DotDash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DotDash
+//
+//  Created by Tiago de Oliveira on 22/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DotDash/DotDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotDashApp.swift
+//  DotDash
+//
+//  Created by Tiago de Oliveira on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DotDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
