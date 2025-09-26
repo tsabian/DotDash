da11a550f2abf66f36641aee520005d252f15dc0
Tiago Oliveira <tsabian@hotmail.com>
2025-09-26 18:43:17 -0300

    adjustimens

---
+++ src/Core/Sources/MorseTable.swift
@@ -0,0 +1,7 @@
+//
+//  MorseTable.swift
+//  Core
+//
+//  Created by Tiago de Oliveira on 26/09/25.
+//
+
--- src/DotDashTests/DotDashTests.swift
+++ src/DotDashTests/DotDashTests.swift
@@ -9,6 +9,5 @@ import Testing
 
 struct DotDashTests {
   @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
   }
 }
